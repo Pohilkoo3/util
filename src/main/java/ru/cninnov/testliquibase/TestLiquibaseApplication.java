80a85846b57778bec9f5a6a98aca16b5b07b72ec
Олег <o.pokhilko@cn-innov.ru>
2023-04-27 18:29:05 +0300

    init

---
+++ src/main/java/ru/cninnov/testliquibase/TestLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package ru.cninnov.testliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLiquibaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cninnov/testliquibase/TestLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cninnov.testliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
